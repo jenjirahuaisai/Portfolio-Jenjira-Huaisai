3613b2776c31ac3f655d7ab91254c0b89f94caf5
jenjirahuaisai <Jenjira.huaisai@stenden.nhlstenden.com>
2025-12-18 21:48:58 +0100

    added images and fonts in 'assets' folder, updated index.php and style.css

--- public/index.php
+++ public/index.php
@@ -1,1 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jenjira Huiasai's Portfolio</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <!-- header logo + nav bar + hero -->
+    <header class="header">
+        <div class="header__container">
+            <!-- logo -->
+            <h3 class="header__logo">JENJIRA</h3>
+
+            <!-- nav -->
+            <nav>
+                <ul>
+                    <li>Home</a></li>
+                    <li>About</a></li>
+                    <li>Projects</a></li>
+                    <li>Portfolio</li>
+                    <li>Contact me</li>
+                    <li><img src="../assets/images/earth.png" alt="language">EN</li>
+                    <li class="signin">Log in</li>
+                </ul>
+            </nav>
+    </header>
+
+    <!-- hero -->
+    <div class="hero">
+        <div class="hero__content">
+            <h3>
+                <&nbsp; /&nbsp;PORTFOLIO&nbsp;>
+            </h3>
+            <br><br>
+            <h1>Jenjira&nbsp;&nbsp;Huaisai</h1>
+            <br><br>
+            <p>
+                [&nbsp;IT&nbsp;STUDENT&nbsp;]&nbsp;[&nbsp;NHL&nbsp;STENDEN&nbsp;HOGESCHOOL&nbsp;]
+            </p>
+        </div>
+    </div>
+
+
+    <!-- ABOUT ME -->
+    <div class="about">
+
+        <div class="about__left">
+            <div class="profile">
+                <img class="profileimg" src="../assets/images/img_profile_jenjira.png" alt="Jenjira's Profile Photo">
+                <p>Jenjira Huaisai</p>
+                <div class="profilesocial">
+                    <img src="../assets/images/facebook.png" alt="facebook">
+                    <img src="../assets/images/instagram.png" alt="instagram">
+                    <img src="../assets/images/linkedin.png" alt="linkedin">
+                </div>
+            </div>
+        </div>
+
+        <div class="about__right">
+            <div class="bio">
+                <h4>ABOUT ME<h4>
+                        <br><br>
+                        <p>I am an IT student of NHL Stenden University of Applied Sciences.
+                        <p>I am eager to join a tech company building out-of-the-box solutions</p>
+                        <p>for international clients. I am skilled in graphic designs, </p>
+                        <p>web development, managing projects, and user documentation.</p>
+
+                        <button>CV</button>&nbsp;
+                        <button>Portfolio</button>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- PROJECTS -->
+
+    <div class="project__container">
+        <div>
+            <h3>PROJECTS</h3>
+        </div>
+
+        <div class="project__grid">
+            <div class="project__item">
+                <img src="../assets/images/graphic.png" alt="graphic">
+                <p>Graphic Designs</p><br>
+                <button>Explore</button>
+            </div>
+
+            <div class="project__item">
+                <img src="../assets/images/webdev.png" alt="graphic">
+                <p>Web Development & Design</p>
+                <button>Explore</button>
+            </div>
+
+            <div class="project__item">
+                <img src="../assets/images/database.png" alt="graphic">
+                <p>Database Management Application</p>
+                <button>Explore</button>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- PORTFOLIO -->
+    <div class="portfolio__container">
+        <h3>PORTFOLIO</h3>
+        <div class="portfolio__item">
+            <h4>Log in</h4>
+            <br>
+            <p>to explore my portfolio</p>
+            <br><br><br>
+            <div class="input-group">
+                <img src="../assets/images/user-icon.png" class="icon-img">
+                <input type="email" placeholder="Email Address">
+            </div>
+
+            <div class="input-group">
+                <img src="../assets/images/key-icon.png" class="icon-img">
+                <input type="password" placeholder="Password">
+            </div>
+            <br>
+
+            <div class="options">
+                <label><input type="checkbox"> &nbsp;Remember me </label>
+                <a href="#">Forgot Password?</a>
+            </div>
+
+            <button class="login__button">LOG IN</button>
+        </div>
+    </div>
+    </div>
+    </div>
+
+    <!-- CONTACT -->
+    <div class="contact__container">
+
+        <div class="contact__left">
+            <h3> CONTACT ME</h3>
+            <br>
+
+            <p><img class="contact__left__img" src="../assets/images/location.png">Ermerveen 17 7814VB Emmen (NL)</p>
+            <p><img class="contact__left__img" src="../assets/images/email.png">jenjira.huaisai@student.nhlstenden.com</p>
+            <br><br>
+            <h4>Social Media</h4>
+
+            <div class="profilesocial2">
+                <img src="../assets/images/facebook.png" alt="facebook">
+                <img src="../assets/images/instagram.png" alt="instagram">
+                <img src="../assets/images/linkedin.png" alt="linkedin">
+            </div>
+        </div>
+
+        <div class="contact__right">
+            <div class="form__container">
+                <label for="">Name:</label>
+                <input class="form__field" type="text">
+                <br>
+                <label for="">Email:</label>
+                <input class="form__field" type="text">
+                <br>
+                <label for="">Role:&nbsp;&nbsp;</label>
+                <input type="radio">&nbsp;Student&nbsp;
+                <input type="radio">&nbsp;Teacher&nbsp;
+                <input type="radio">&nbsp;Other
+                <br>
+
+                <label for="">Message:</label><br>
+                <textarea name="" id=""></textarea>
+                <br>
+                <input class="form__button" type="SUBMIT">&nbsp;&nbsp;
+                <input class="form__button" type="RESET">
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <div class="footer__container">
+        <p>&copy;2025 JENJIRA - All rights reserved</p>
+    </div>
+
+</body>
+
+</html>
